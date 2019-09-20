7f24915cff26d6c12e32e23ba1bb43ef341d2e28
Fedor Taskaev <f.taskaev@propellads.net>
2019-09-20 03:08:54 +0300

    Fix repo key

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-git clone -b monolith https://github.com/express42/reddit.git /opt/puma-server
+sudo git clone -b monolith https://github.com/express42/reddit.git /opt/puma-server
 cd /opt/puma-server && bundle install
 
-/opt/puma-server/puma -d
+puma -d
 
